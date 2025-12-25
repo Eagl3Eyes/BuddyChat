c8c30fb1067cd4ccaebca63fcc8f928676677de5
Tuhin AJ <tuhinjobayergolap007@gmail.com>
2025-12-26 02:04:26 +0600

    ready to deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -9,30 +8,20 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.json());
-
-/* ---- FRONTEND ---- */
-if (process.env.NODE_ENV === "production") {
-    const frontendPath = path.join(__dirname, "../../frontend/dist");
-
-    console.log("Serving frontend from:", frontendPath);
-
-    app.use(express.static(frontendPath));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(frontendPath, "index.html"));
-    });
-}
-
-/* ---- API ---- */
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get((_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+app.listen(PORT, () => console.log("Server running on port " + PORT))
