c070bb3cf2d75347d26c7c2fa656928d5e99c398
Tuhin AJ <tuhinjobayergolap007@gmail.com>
2025-12-26 07:53:21 +0600

    update profile endpoint done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,15 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { ENV } from "../lib/env.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
-    const { fullname, email, password } = req.body
+    const { fullName, email, password } = req.body;
 
     try {
-        if (!fullname || !email || !password) {
+        if (!fullName || !email || !password) {
             return res.status(400).json({ message: "All fields are required" });
         }
 
@@ -13,7 +17,7 @@ export const signup = async (req, res) => {
             return res.status(400).json({ message: "Password must be at least 6 characters" });
         }
 
-        // check if email is valid: regex
+        // check if emailis valid: regex
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)) {
             return res.status(400).json({ message: "Invalid email format" });
@@ -22,7 +26,7 @@ export const signup = async (req, res) => {
         const user = await User.findOne({ email });
         if (user) return res.status(400).json({ message: "Email already exists" });
 
-        // password hash
+        // 123456 => $dnjasdkasj_?dmsakmk
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -51,8 +55,6 @@ export const signup = async (req, res) => {
         } else {
             res.status(400).json({ message: "Invalid user data" });
         }
-
-
     } catch (error) {
         console.log("Error in signup controller:", error);
         res.status(500).json({ message: "Internal server error" });
